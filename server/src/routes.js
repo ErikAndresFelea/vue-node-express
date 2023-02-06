f065628cd32306d9c3ea0aada9ad777fe905195f
ErikAndresFelea <erikandresfelea@gmail.com>
2023-02-06 09:49:32 +0100

    Structure changes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 
 module.exports = (app) => {
-    dbPath = 'C:/Users/remoA/Desktop/VSC Proyects/fullstack-project/server/db-pruebas.json'
+    databaseFile = 'C:/Users/Argnos/Desktop/fullstack-project/server/db-pruebas.json'
 
     // Obtain the transcriptions
     app.get('/transcriptions', (req, res) => {
-        fs.readFile(dbPath, 'utf8', (err, data) => {
+        fs.readFile(databaseFile, 'utf8', (err, data) => {
             if (err) res.status(500).send({ error: err })
             else res.send(JSON.parse(data))
         })
@@ -13,7 +13,7 @@ module.exports = (app) => {
 
     // Add a transcription
     app.post('/transcriptions', (req, res) => {
-        fs.readFile(dbPath, 'utf8', (err, data) => {
+        fs.readFile(databaseFile, 'utf8', (err, data) => {
             if (err) return res.status(500).send(err)
 
             // Obtain max id from json
@@ -25,7 +25,7 @@ module.exports = (app) => {
             jsonData.push(transcription)
 
             // Write on the json
-            fs.writeFile(dbPath, JSON.stringify(jsonData), err => {
+            fs.writeFile(databaseFile, JSON.stringify(jsonData), err => {
                 if (err) return res.status(500).send(err)
                 res.status(201).send('Transcripcion creada')
             })
@@ -41,8 +41,8 @@ module.exports = (app) => {
     app.delete('/transcriptions/:id', (req, res) => {
         const id = req.params.id
 
-        fs.readFile(dbFile, (err, data) => {
-            if (err) req.status(500).send({ message: err })
+        fs.readFile(databaseFile, (err, data) => {
+            if (err) res.status(500).send({ message: err })
 
             // Search for the transcription with id given
             const jsonData = JSON.parse(data)
@@ -52,7 +52,7 @@ module.exports = (app) => {
 
             // Add the updated info to the json
             jsonData.splice(index, 1)
-            fs.writeFile(dbFile, JSON.stringify(jsonData), (err) => {
+            fs.writeFile(databaseFile, JSON.stringify(jsonData), (err) => {
                 if (err) res.status(500).send({ msg: err })
                 res.status(200).send({ message: 'Transcripcion eliminada' })
             })
@@ -61,6 +61,6 @@ module.exports = (app) => {
 
     // Prueba
     app.get('/db-pruebas', (req, res) => {
-        res.sendFile(dbPath)
+        res.sendFile(databaseFile)
     })
 }
