81d145760622e14905ea0658e6c4cb2731448902
ErikAndresFelea <erikandresfelea@gmail.com>
2023-02-07 11:18:05 +0100

    Added get by id

--- server/src/routes.js
+++ server/src/routes.js
@@ -11,24 +11,43 @@ module.exports = (app) => {
         })
     })
 
+    // Obtain a specific transcription
+    app.get('/transcriptions/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        fs.readFile(databaseFile, 'utf8', (err, data) => {
+            if (err) res.status(500).send({ error: err })
+            else {
+                // Obtain the specific object
+                const jsonData = JSON.parse(data)
+                const transc = jsonData.find(t => t.id === id)
+
+                if (!transc) res.status(404).send({ message: 'Transcripcion no encontrada' })
+                else res.send(transc)
+            }
+        })
+    })
+
     // Add a transcription
     app.post('/transcriptions', (req, res) => {
         fs.readFile(databaseFile, 'utf8', (err, data) => {
             if (err) return res.status(500).send(err)
 
-            // Obtain max id from json
-            const jsonData = JSON.parse(data)
-            const maxId = jsonData.reduce((max, t) => Math.max(max, t.id), 0)
-
-            // Add the transcription to the json array
-            const transcription = { id: maxId + 1, ...req.body }
-            jsonData.push(transcription)
-
-            // Write on the json
-            fs.writeFile(databaseFile, JSON.stringify(jsonData, null, 4), err => {
-                if (err) return res.status(500).send(err)
-                res.status(201).send('Transcripcion creada')
-            })
+            else {
+                // Obtain max id from json
+                const jsonData = JSON.parse(data)
+                const maxId = jsonData.reduce((max, t) => Math.max(max, t.id), 0)
+                
+                // Add the transcription to the json array
+                const transcription = { id: maxId + 1, ...req.body }
+                jsonData.push(transcription)
+                
+                // Write on the json
+                fs.writeFile(databaseFile, JSON.stringify(jsonData, null, 4), err => {
+                    if (err) return res.status(500).send(err)
+                    res.status(201).send('Transcripcion creada')
+                })
+            }
         })
     })
 
@@ -39,20 +58,22 @@ module.exports = (app) => {
 
         fs.readFile(databaseFile, (err, data) => {
             if (err) res.status(500).send({ message: err})
-            
-            // Search for the transcription with id given
-            const jsonData = JSON.parse(data)
-            const index = jsonData.findIndex(t => t.id === id)
 
-            if (index === -1) res.status(404).send({ message: 'Transcripcion no encontrada' })
-
-            // Add the updated info to the json
             else {
-                jsonData[index] = { ...jsonData[index], ...body }
-                fs.writeFile(databaseFile, JSON.stringify(jsonData, null, 4), (err) => {
-                    if (err) res.status(500).send({ msg: err })
-                    res.status(200).send({ message: 'Transcripcion modificada' })
-                })
+                // Search for the transcription with id given
+                const jsonData = JSON.parse(data)
+                const index = jsonData.findIndex(t => t.id === id)
+                
+                if (index === -1) res.status(404).send({ message: 'Transcripcion no encontrada' })
+                
+                // Add the updated info to the json
+                else {
+                    jsonData[index] = { ...jsonData[index], ...body }
+                    fs.writeFile(databaseFile, JSON.stringify(jsonData, null, 4), (err) => {
+                        if (err) res.status(500).send({ msg: err })
+                        res.status(200).send({ message: 'Transcripcion modificada' })
+                    })
+                }
             }
         })
     })
@@ -64,19 +85,21 @@ module.exports = (app) => {
         fs.readFile(databaseFile, (err, data) => {
             if (err) res.status(500).send({ message: err})
 
-            // Search for the transcription with id given
-            const jsonData = JSON.parse(data)
-            const index = jsonData.findIndex(t => t.id === id)
-
-            if (index === -1) res.status(404).send({ message: 'Transcripcion no encontrada' })
-
-            // Add the updated info to the json
             else {
-                jsonData.splice(index, 1)
-                fs.writeFile(databaseFile, JSON.stringify(jsonData, null, 4), (err) => {
-                    if (err) res.status(500).send({ msg: err })
-                    res.status(200).send({ message: 'Transcripcion eliminada' })
-                })
+                // Search for the transcription with id given
+                const jsonData = JSON.parse(data)
+                const index = jsonData.findIndex(t => t.id === id)
+                
+                if (index === -1) res.status(404).send({ message: 'Transcripcion no encontrada' })
+                
+                // Add the updated info to the json
+                else {
+                    jsonData.splice(index, 1)
+                    fs.writeFile(databaseFile, JSON.stringify(jsonData, null, 4), (err) => {
+                        if (err) res.status(500).send({ msg: err })
+                        res.status(200).send({ message: 'Transcripcion eliminada' })
+                    })
+                }
             }
         })
     })
